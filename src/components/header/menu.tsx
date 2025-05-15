a2c86dab377cc7c8e788f014e5ccc5116a1c185e
zhangzhang88 <68057668+zhangzhang88@users.noreply.github.com>
2025-05-15 08:37:51 +0800

    Update menu.tsx

--- src/components/header/menu.tsx
+++ src/components/header/menu.tsx
@@ -13,27 +13,11 @@ function ThemeToggle() {
 }
 
 export function Menu() {
-  const { loggedIn, login, logout, userInfo, enableLogin } = useLogin()
   const [shown, show] = useState(false)
   return (
     <span className="relative" onMouseEnter={() => show(true)} onMouseLeave={() => show(false)}>
       <span className="flex items-center scale-90">
-        {
-          enableLogin && loggedIn && userInfo.avatar
-            ? (
-                <button
-                  type="button"
-                  className="h-6 w-6 rounded-full bg-cover"
-                  style={
-                    {
-                      backgroundImage: `url(${userInfo.avatar}&s=24)`,
-                    }
-                  }
-                >
-                </button>
-              )
-            : <button type="button" className="btn i-si:more-muted-horiz-circle-duotone" />
-        }
+        <button type="button" className="btn i-si:more-muted-horiz-circle-duotone" />
       </span>
       {shown && (
         <div className="absolute right-0 z-99 bg-transparent pt-4 top-4">
@@ -51,42 +35,11 @@ export function Menu() {
             }}
           >
             <ol className="bg-base bg-op-70! backdrop-blur-md p-2 rounded-lg color-base text-base">
-              {enableLogin && (loggedIn
-                ? (
-                    <li onClick={logout}>
-                      <span className="i-ph:sign-out-duotone inline-block" />
-                      <span>退出登录</span>
-                    </li>
-                  )
-                : (
-                    <li onClick={login}>
-                      <span className="i-ph:sign-in-duotone inline-block" />
-                      <span>Github 账号登录</span>
-                    </li>
-                  ))}
+              <li onClick={() => window.open("https://ztr8.online/")} className="cursor-pointer [&_*]:cursor-pointer transition-all">
+                <span className="i-ph:link-duotone inline-block" />
+                <span>未来已到</span>
+              </li>
               <ThemeToggle />
-              <li onClick={() => window.open(Homepage)} className="cursor-pointer [&_*]:cursor-pointer transition-all">
-                <span className="i-ph:github-logo-duotone inline-block" />
-                <span>Star on Github </span>
-              </li>
-              <li className="flex gap-2 items-center">
-                <a
-                  href="https://github.com/ourongxing/newsnow"
-                >
-                  <img
-                    alt="GitHub stars badge"
-                    src="https://img.shields.io/github/stars/ourongxing/newsnow?logo=github"
-                  />
-                </a>
-                <a
-                  href="https://github.com/ourongxing/newsnow/fork"
-                >
-                  <img
-                    alt="GitHub forks badge"
-                    src="https://img.shields.io/github/forks/ourongxing/newsnow?logo=github"
-                  />
-                </a>
-              </li>
             </ol>
           </motion.div>
         </div>
